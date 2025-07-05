80670a7c0bb0016c23091c8d36c46b70b493f14d
Avi <115152362+AviGawande@users.noreply.github.com>
2025-07-05 20:39:39 +0530

    Delete build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appHexaCircles_FILE D:/projects/qtcreator/HexaCircles/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/appHexaCircles.exe)
-set(__QT_DEPLOY_TARGET_appHexaCircles_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appHexaCircles_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
